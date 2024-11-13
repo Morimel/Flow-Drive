26ad8485d7435d256d32fb2131b68b2cdd3e03a4
Morimel <melsovisa77@gmail.com>
2024-11-13 20:21:36 +0600

    Initial Commit

---
+++ petTests/petTests.swift
@@ -0,0 +1,17 @@
+//
+//  petTests.swift
+//  petTests
+//
+//  Created by Isa Melsov on 13/11/24.
+//
+
+import Testing
+@testable import pet
+
+struct petTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
